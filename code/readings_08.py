f9344686af53dc57d418b429b4c7aabac2827774
Rob Fatland <rob5@uw.edu>
2024-01-09 18:51:56 -0800

    update 2024

--- code/readings_08.py
+++ code/readings_08.py
@@ -4,11 +4,12 @@ import numpy
 def main():
     script = sys.argv[0]
     if len(sys.argv) == 1:  # no arguments, so print help message
-        print('Usage: python readings_08.py action filenames\n'
-              'action must be one of --min --mean --max\n'
-              'if filenames is blank, input is taken from stdin;\n'
-              'otherwise, each filename in the list of arguments\n'
-              'is processed in turn')
+        print("Usage: python readings_08.py action filenames\n"
+              "Action:\n"
+              "    Must be one of --min, --mean, or --max.\n"
+              "Filenames:\n"
+              "    If blank, input is taken from standard input (stdin).\n"
+              "    Otherwise, each filename in the list of arguments is processed in turn.")
         return
 
     action = sys.argv[1]
@@ -25,11 +26,11 @@ def process(filename, action):
     data = numpy.loadtxt(filename, delimiter=',')
 
     if action == '--min':
-        values = numpy.min(data, axis=1)
+        values = numpy.amin(data, axis=1)
     elif action == '--mean':
         values = numpy.mean(data, axis=1)
     elif action == '--max':
-        values = numpy.max(data, axis=1)
+        values = numpy.amax(data, axis=1)
 
     for val in values:
         print(val)
